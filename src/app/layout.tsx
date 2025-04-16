58ef321ccd1727e5f1a0b48c94e6c6911a4bf1bd
LittleBarista <chaplinrus@gmail.com>
2025-04-16 15:18:38 +0300

    Добавлен счетчик Яндекс.Метрики

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -73,6 +74,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru" className={inter.variable}>
+      <head>
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(101111714, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+            });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/101111714" style={{position:'absolute', left:'-9999px'}} alt="" />
+          </div>
+        </noscript>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
